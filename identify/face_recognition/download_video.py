a488b2dd8b523d35a68e9394c7f81350d34dd232
JuYunCheng <juyun.cheng@sjsu.edu>
2019-11-21 23:50:23 -0800

    add High Revolution Option

--- identify/face_recognition/download_video.py
+++ identify/face_recognition/download_video.py
@@ -3,6 +3,7 @@ import os
 import logging
 import uuid
 
+high_resolution = False  #True or False
 video_path = 'identify/face_recognition/video/'
 
 def download_video_from_YoutubeURL(url, new_file):
@@ -11,7 +12,10 @@ def download_video_from_YoutubeURL(url, new_file):
 	video_url = url
 	youtube = pytube.YouTube(video_url)
 	logging.info("download: %s", youtube.title)
-	video = youtube.streams.filter(progressive = True,file_extension='mp4').first()
+	if high_resolution:
+		video = youtube.streams.filter(adaptive=True,file_extension='mp4').first()
+	else : 
+		video = youtube.streams.filter(progressive=True,file_extension='mp4').first()
 	logging.info(video)
 	video.download(video_path, filename=new_file)
 	logging.info("=== finish dowloading ===")
