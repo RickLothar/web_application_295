f21ff0e15a507c26e42455d9abeb5043e890cc1e
Jin, Shikai <jinshikai@gmail.com>
2019-11-22 15:31:44 -0800

    adjust the max length of model

--- identify/models.py
+++ identify/models.py
@@ -25,8 +25,8 @@ class Result(models.Model):
     videoonline = models.ForeignKey('VideoOnline', on_delete=models.SET_NULL, null=True)
     video =  models.ForeignKey('Video', on_delete=models.SET_NULL, null=True, related_name='input_video')
     output =  models.ForeignKey('Video', on_delete=models.SET_NULL, null=True, related_name='output_video')
-    target_name = models.CharField(max_length=200)
-    target_result = models.CharField(max_length=200)
+    target_name = models.CharField(max_length=1024)
+    target_result = models.CharField(max_length=1024)
     def __str__(self):
         return f'{self.id}'
     def get_absolute_url(self):
@@ -35,7 +35,7 @@ class Result(models.Model):
 class Video(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular video')
     channel = models.ForeignKey(Channel, on_delete=models.SET_NULL, null=True)
-    file_name = models.CharField(max_length=200)
+    file_name = models.CharField(max_length=1024)
     def __str__(self):
         return f'{self.id}'
     def get_absolute_url(self):
